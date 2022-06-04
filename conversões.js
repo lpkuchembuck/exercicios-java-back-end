1017d4b846d53231f68abc243226f90784560ecc
lpkuchembuck <lpkuchembuck@gmail.com>
2022-06-04 00:03:43 -0300

    Adicionando arquivo conversão.js

---
+++ conversões.js
@@ -0,0 +1,18 @@
+// conversão implicita
+
+const numero = 456;
+const numerostring = "456";
+
+
+//Number[]
+//String[]
+console.log(numero===numerostring)
+console.log(numero==numerostring)
+console.log(numero+numerostring)
+
+// conversão explicita
+//Number()
+//String()
+
+console.log(numero+Number(numerostring))
+
