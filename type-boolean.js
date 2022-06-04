b612c4f11a9c55108058811dd766acac02c09558
lpkuchembuck <lpkuchembuck@gmail.com>
2022-06-03 23:50:19 -0300

    Adicionando mais exemplos no arquivo de booleans

--- type-boolean.js
+++ type-boolean.js
@@ -7,3 +7,31 @@ const texto1 = "testando";
 const texto2 = "testandoo";
 
 console.log(texto1===texto2);
+
+// boolean
+
+const usuariologado = true;
+const contapaga = false;
+
+// truthy ou falsy
+
+//0 => false
+//1 => true
+
+console.log(0==false)
+console.log(""==false)
+console.log(1==true)
+
+// null = representa vazio, nada
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar)
+console.log(varNull)
+
+let numero = 3;
+let texto = "alura";
+
+console.log(typeof minhaVar)
+console.log(typeof varNull)
