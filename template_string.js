18d1f5d131268812ce6b44adefe237a177081bd2
lpkuchembuck <lpkuchembuck@gmail.com>
2022-06-11 18:01:19 -0300

    Adicionando arquivo de ternário

---
+++ template_string.js
@@ -0,0 +1,12 @@
+const nome = "Luiz Paulo";
+const idade = "36";
+const cidadeNascimento = "São Paulo";
+
+//const apresentacao = "Meu nome é " + nome + ", minha idade é " 
++ idade + " anos" + " e nasci na cidade de " + cidadeNascimento;
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} 
+e nasci na cidade de ${cidadeNascimento}.`
+
+
+console.log(apresentacao);
